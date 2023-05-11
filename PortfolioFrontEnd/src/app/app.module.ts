d067d0af3192169449a2148c6f3f8afcc1672de3
AniArrieta <arrietar.ana@gmail.com>
2023-05-11 18:29:02 +0200

    crear anclaje con backend

--- PortfolioFrontEnd/src/app/app.module.ts
+++ PortfolioFrontEnd/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { LogoComponent } from './components/logo/logo.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FooterComponent } from './components/footer/footer.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
